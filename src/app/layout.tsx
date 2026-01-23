f04eabeac2225b256ed6d19578429edde0e2f7ea
HoneyShot <mfatih.gok.045@gmail.com>
2026-01-23 12:41:16 +0300

    Extra Updates for View

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: "Profuture Teknoloji Dergisi",
@@ -16,12 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" className="overflow-x-hidden">
+      <head>
+        <meta name="google-site-verification" content="RUP4QWHtMEWCEQCb3UaWeSUHwIMwVicraT0Y2NhdQkc" />
+      </head>
       <body className="min-h-screen flex flex-col overflow-x-hidden">
         <Header />
         <main className="flex-1">
           {children}
         </main>
         <Footer />
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
       </body>
     </html>
   );
